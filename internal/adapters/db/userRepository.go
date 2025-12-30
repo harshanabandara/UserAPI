0c36131167a61dfeabc24379d1258904d691ffb7
Harshana <e16049@eng.pdn.ac.lk>
2025-12-30 12:10:10 +0530

    Added db implementation. Will do sqlc query building after converting the project into multi-module workspace.

--- internal/adapters/db/userRepository.go
+++ internal/adapters/db/userRepository.go
@@ -3,6 +3,9 @@ package db
 import (
 	"UserApi/internal/core/domain"
 	"database/sql"
+	"errors"
+	"fmt"
+	"strings"
 )
 
 type UserRepositoryImpl struct {
@@ -13,31 +16,127 @@ func NewSQLUserRepository(db *sql.DB) *UserRepositoryImpl {
 	return &UserRepositoryImpl{db: db}
 }
 
-func (u *UserRepositoryImpl) RetrieveUser(s string) (domain.User, error) {
+func (u *UserRepositoryImpl) RetrieveUser(userID string) (domain.User, error) {
+	query := "SELECT user_id, first_name, last_name, email, age, status FROM users WHERE user_id = $1"
 
-	panic("implement me")
+	var user domain.User
+	var userStatus string
+	var active = domain.ACTIVE
+	err := u.db.QueryRow(query, userID).Scan(
+		&user.UserID, &user.FirstName, &user.LastName, &user.Email, &user.Age, &userStatus,
+	)
+	switch userStatus {
+	case active.String():
+		user.Status = domain.ACTIVE
+	default:
+		user.Status = domain.INACTIVE
+	}
+	if err != nil {
+		return domain.User{}, err
+	}
+	return user, nil
 }
 
 func (u *UserRepositoryImpl) CreateUser(user domain.User) (domain.User, error) {
+	query := "INSERT INTO users (first_name, last_name, email, age, phone) VALUES ($1, $2, $3, $4, $5) RETURNING user_id"
+	var id string
 
-	panic("implement me")
+	err := u.db.QueryRow(query, user.FirstName, user.LastName, user.Email, user.Age, user.Phone).Scan(&id)
+	if err != nil {
+		return domain.User{}, err
+	}
+	user.UserID = id
+	return user, nil
 }
 
 func (u *UserRepositoryImpl) UpdateUser(userId string, user domain.User) (domain.User, error) {
+	if userId == "" {
+		return domain.User{}, errors.New("user id is required")
+	}
+	fields := []string{}
+	vals := []interface{}{}
+	if user.FirstName != "" {
+		fields = append(fields, fmt.Sprintf("first_name = $%d", len(fields)+1))
+		vals = append(vals, user.FirstName)
+	}
+	if user.LastName != "" {
+		fields = append(fields, fmt.Sprintf("last_name = $%d", len(fields)+1))
+		vals = append(vals, user.LastName)
+	}
+	if user.Email != "" {
+		fields = append(fields, fmt.Sprintf("email = $%d", len(fields)+1))
+		vals = append(vals, user.Email)
+	}
+	if user.Age != 0 {
+		fields = append(fields, fmt.Sprintf("age = $%d", len(fields)+1))
+		vals = append(vals, user.Age)
+	}
+	if user.Phone != "" {
+		fields = append(fields, fmt.Sprintf("phone = $%d", len(fields)+1))
+		vals = append(vals, user.Phone)
+	}
+	if user.Status != 0 {
+		fields = append(fields, fmt.Sprintf("status = $%d", len(fields)+1))
+		vals = append(vals, user.Status.String())
+	}
+	vals = append(vals, userId)
+	// build the string
+	updatedUser := domain.User{}
+	var status string
+	var inactive = domain.INACTIVE
+	query := fmt.Sprintf("UPDATE users SET %s WHERE user_id = $%d RETURNING user_id,first_name, last_name, email, age, phone, status", strings.Join(fields, ","), len(fields)+1)
+	err := u.db.QueryRow(query, vals...).Scan(
+		&updatedUser.UserID, &updatedUser.FirstName, &updatedUser.LastName, &updatedUser.Email, &updatedUser.Age, &updatedUser.Phone, &status)
+	if err != nil {
+		return domain.User{}, err
+	}
+	switch status {
+	case inactive.String():
+		updatedUser.Status = domain.INACTIVE
+	default:
+		updatedUser.Status = domain.ACTIVE
+	}
+	return updatedUser, nil
 
-	panic("implement me")
 }
 
 func (u *UserRepositoryImpl) DeleteUser(userId string) error {
-
-	panic("implement me")
+	query := "DELETE FROM users WHERE user_id = $1"
+	_, err := u.db.Exec(query, userId)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (u *UserRepositoryImpl) Init() error {
-
-	panic("implement me")
+	return nil
 }
 
 func (u *UserRepositoryImpl) RetrieveAllUsers() ([]domain.User, error) {
-	panic("implement me")
+	query := "SELECT user_id, first_name, last_name, email, age, status FROM users"
+	rows, err := u.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []domain.User
+	var inactive = domain.INACTIVE
+	for rows.Next() {
+		var u domain.User
+		var userStatus string
+		if err := rows.Scan(&u.UserID, &u.FirstName, &u.LastName, &u.Email, &u.Age, &userStatus); err != nil {
+			return nil, err
+		}
+
+		switch userStatus {
+		case inactive.String():
+			u.Status = domain.INACTIVE
+		default:
+			u.Status = domain.ACTIVE
+		}
+		users = append(users, u)
+	}
+	return users, nil
 }
--- main.go
+++ main.go
@@ -5,12 +5,37 @@ import (
 	"UserApi/internal/adapters/http"
 	"UserApi/internal/adapters/service"
 	"UserApi/internal/core/ports"
+	"database/sql"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
 )
 
+func getEnv(key, fallback string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return fallback
+}
 func main() {
-	var userRepository ports.UserRepository = db.NewMockUserRepository()
+	host := getEnv("PG_HOST", "localhost")
+	port := getEnv("PG_PORT", "5432")
+	user := getEnv("PG_USER", "postgres")
+	password := getEnv("PG_PASSWORD", "yaalalabs")
+	databaseName := getEnv("PG_DATABASE", "userapi")
+	sslmode := getEnv("PG_SSLMODE", "disable")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, databaseName, sslmode)
+	database, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	var userRepository ports.UserRepository = db.NewSQLUserRepository(database)
 	var userService ports.UserService = service.NewUserService(userRepository)
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
@@ -18,8 +43,9 @@ func main() {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.StripSlashes)
 	var server = http.Server{UserService: userService, Router: r}
-	err := server.Start()
+	err = server.Start()
 	if err != nil {
 		return
 	}
+
 }
